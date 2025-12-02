37dec71bce3f2bf4feed0e22699ee2e8f0c3b0e7
Rasmus Melgaard Villumsen <rav001@edu.zealand.dk>
2025-12-02 12:43:37 +0100

    Added some classes and a interface

---
+++ Hillerød Sejlklub/IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hillerød_Sejlklub
+{
+    internal interface IPerson
+    {
+        string Name { get; set; }
+    }
+}
