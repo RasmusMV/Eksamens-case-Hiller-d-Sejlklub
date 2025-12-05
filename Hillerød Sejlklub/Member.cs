438ed063bd0f30baadd053ffadc6753287f7a3e6
Lennnoss <lennartnice2@gmail.com>
2025-12-05 13:52:34 +0100

    Member class, UserRepository, IUserRepository

--- Hillerød Sejlklub/Member.cs
+++ Hillerød Sejlklub/Member.cs
@@ -7,8 +7,26 @@ using Hillerød_Sejlklub.Interfaces;
 
 namespace Hillerød_Sejlklub
 {
-    public class Member : IUserRepository
+    public class Member
     {
-        public string Name { get; set; }
+        protected string _name;
+
+
+        public Member(string name, int age, int id, string mail, int phoneNumber)
+        {
+            _name = name;
+            Age = age;
+            ID = id;
+            Mail = mail;
+            PhoneNumber = phoneNumber;
+
+        }
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Age { get; set; }
+        public int ID { get; set; }
+        public string Mail { get; set; }
+        public int PhoneNumber { get; set; }
     }
+
+
 }
--- Hillerød Sejlklub/Repositories/UserRepository.cs
+++ Hillerød Sejlklub/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hillerød_Sejlklub.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,55 @@ using System.Threading.Tasks;
 
 namespace Hillerød_Sejlklub.Repositories
 {
-    public class UserRepository
+    public class MemberRepository : IUserRepository
     {
+        private readonly Dictionary<string, Member> _members;
 
+        public MemberRepository()
+        {
+            _members = new Dictionary<string, Member>
+        {
+                {"Jan", new Member("Jan", 52,  110100, "JanErSej@gmail.com", 20202020) },
+                {"Erik", new Member("Erik", 67,  110101, "ErikErSej@gmail.com", 20202021) },
+                {"Buller", new Member("Buller", 55,  110102, "BullerErSej@gmail.com", 20202022) }
+        };
+
+        }
+
+        public Member Add(Member member)
+        {
+            _members.Add(member.Name, member);
+            return member;
+        }
+
+        public bool Delete(string memberName)
+        {
+            if (_members.Remove(memberName))
+                return true;
+            else throw new Exception($"{memberName} could not be found");
+        }
+
+        public List<Member> GetAll()
+        {
+            return _members.Values.ToList();
+        }
+
+        public Member GetByName(string name)
+        {
+            return _members[name];
+        }
+
+        public Member Update(Member member, string key)
+        {
+            _members.Remove(key);
+            var updatedMember = Add(member);
+
+            return updatedMember;
+        }
+
+        List<Member> IUserRepository.GetAll()
+        {
+            return _members.Values.ToList();
+        }
     }
 }
