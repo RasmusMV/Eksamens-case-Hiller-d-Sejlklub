33b223d36872cad74b02bf42c541324332d53a13
RasmusMV <rav001@edu.zealand.dk>
2025-12-05 13:25:45 +0100

    Created a booking repository, and a BookingDateException. Created the Booking constructor as well as the accompanying properties, as well as 2 private methods to help create booking objects. Created the BookingRepository constructor and made it a singleton design, and for creating and getting the class a GetInstance method that at first initializes an instance of the class, and then returns the same instance if called again. Also created Read, Add, Delete and update methods to the repository

--- Hillerød Sejlklub/Booking.cs
+++ Hillerød Sejlklub/Booking.cs
@@ -3,10 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hillerød_Sejlklub.Repositories;
+using Hillerød_Sejlklub.Exceptions;
 
 namespace Hillerød_Sejlklub
 {
     public class Booking
     {
+        #region instance fields
+        private static int _id;
+        #endregion
+
+        #region constructor
+        public Booking(Member member, Boat boat, DateTime dateStart, DateTime dateFinish)
+        {
+            Id = MakeId();
+            Member = member;
+            Boat = boat;
+            try
+            {
+                DateValidation(dateStart, dateFinish);
+            }
+            catch(BookingDateTakenException b)
+            {
+                Console.WriteLine(b);
+            }
+            DateStart = dateStart;
+            DateFinish = dateFinish;
+        }
+        #endregion
+
+        #region properties
+        public Member Member { get; }
+
+        public Boat Boat { get; set; }
+
+        public int Id { get; }
+
+        public DateTime DateStart { get; set; }
+
+        public DateTime DateFinish { get; set; }
+        #endregion
+
+        #region methods
+        private int MakeId()
+        {
+            int newId = _id;
+            _id = _id++;
+            return newId;
+        }
+
+        private void DateValidation(DateTime dateStart, DateTime dateFinish)
+        {
+            if(dateStart >= DateTime.Now && dateFinish > dateStart)
+            {
+                foreach (var booking in BookingRepository.GetInstance().BookingList)
+                {
+                    if (Boat == booking.Value.Boat && dateStart < booking.Value.DateFinish && booking.Value.DateStart < dateFinish)
+                    {
+                        throw new BookingDateTakenException("Your chosen time slot is already occupied");
+                    }
+                }
+            }
+        }
+        #endregion
+
     }
 }
---
+++ Hillerød Sejlklub/Exceptions/BookingDateTakenException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hillerød_Sejlklub.Exceptions
+{
+    public class BookingDateTakenException : Exception
+    {
+        public BookingDateTakenException(string message) : base(message){ }
+
+    }
+}
