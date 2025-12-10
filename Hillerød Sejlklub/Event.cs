598e753d01792b7cf9ae1011e515e6aecd13ac4a
RasmusMV <rav001@edu.zealand.dk>
2025-12-10 11:40:30 +0100

    Added two EventRepository update methods

--- Hillerød Sejlklub/Event.cs
+++ Hillerød Sejlklub/Event.cs
@@ -15,6 +15,7 @@ namespace Hillerød_Sejlklub
             Name = eventName;
             Id = MakeId();
             Description = eventDescription;
+            CreationDate = DateTime.Now;
         }
         
         public string Name { get; set; }
@@ -22,6 +23,8 @@ namespace Hillerød_Sejlklub
         public int Id { get; }
 
         public string Description { get; set; }
+
+        public DateTime CreationDate { get; set; }
 
         private int MakeId()
         {
