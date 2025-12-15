50ecba1d0ab0c95abeb5243c07e6a2286bdabb38
RasmusMV <rav001@edu.zealand.dk>
2025-12-16 00:32:24 +0100

    Removed an unneeded List from Boat, made sure properties and methods are correctly placed in News and also created a ToString method in News. Updated naming scheme of EventList to EventDictionary to avoid confusion, and therefore changed the test code a little

--- Hillerød Sejlklub/News.cs
+++ Hillerød Sejlklub/News.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Hillerød_Sejlklub
 {
@@ -19,15 +20,22 @@ namespace Hillerød_Sejlklub
             Description = newsDescription;
             CreationDate = DateTime.Now;
         }
+       
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Id { get; }
+        public DateTime CreationDate { get; }
+
         private int MakeId()
         {
             int newId = _id;
             _id = _id + 1;
             return newId;
         }
-        public DateTime CreationDate { get; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int Id { get; }
+
+        public override string ToString()
+        {
+            return $"Event: {Name}, Time of posting {CreationDate} \nDescription: {Description}";
+        }
     }
 }
