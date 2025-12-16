862e412f3cdcfbe75017eb568d100bb6e1b7fc57
Lennnoss <lennartnice2@gmail.com>
2025-12-16 12:09:59 +0100

    Tilføjet de forskellige sider samt header + næsten færdig footer

---
+++ Hiller-dSejlklubWebsite-master/HillerødSejlklubWebsite/Pages/Book en båd.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødSejlklubWebsite.Pages
+{
+    public class Book_en_bådModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Hiller-dSejlklubWebsite-master/HillerødSejlklubWebsite/Pages/Lokationsregler.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødSejlklubWebsite.Pages
+{
+    public class LokationsreglerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
